708a6be0e6411e2fc9bb81f8e3cc33dd0cce259e
John-v77 <john.vs11@hotmail.com>
2021-11-29 21:25:15 -0500

    first query

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-
+const {graphqlHTTP} = require('express-graphql')
+const {buildSchema} = require('graphql')
 
 const app = express()
 
 app.use(bodyParser.json())
 
-app.get('/', (req, res, next) => {
-    res.send('Hello World!')
-})
+app.use('/graphql', graphqlHTTP({
+    // /the below schema is in a string
+    schema: buildSchema(`
+    
+        type RootQuery {
+            events:[String!]!
+        }
+
+        type RootMutation{
+            createEvent(name:String):String
+        }
+        schema {
+            query   :   RootQuery
+            mutation:   RootMutation
+        }`),
+    rootValue: {
+        events: _=> ['Cooking Classes', 'Hiking'],
+        createEvent: (args)=>{
+            const eventName = args.name
+            return eventName
+        } 
+    },
+    graphiql: true
+}))
 
 
 app.listen(3000)
