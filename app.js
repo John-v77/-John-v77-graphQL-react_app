0288a41edf3318446855c08e380f37f248def44d
John-v77 <john.vs11@hotmail.com>
2021-12-05 20:23:27 -0500

    connects to data base - MongoDB

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const {graphqlHTTP} = require('express-graphql')
 const {buildSchema} = require('graphql')
+const mongoose = require('mongoose')
+
 
 const app = express()
 
@@ -57,5 +59,7 @@ app.use('/graphql', graphqlHTTP({
     graphiql: true
 }))
 
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.rhd9l.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`)
+.then(console.log('connected to dataBase'), _=> app.listen(3000))
+.catch(err => {console.log(err)})
 
-app.listen(3000)
