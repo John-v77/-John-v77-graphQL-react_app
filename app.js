c7a9cd8b6e617855e3b25ed87f9a844699d23a34
John-v77 <john.vs11@hotmail.com>
2021-12-06 20:40:24 -0500

    add mutation for Database

--- app.js
+++ app.js
@@ -3,12 +3,9 @@ const bodyParser = require('body-parser')
 const {graphqlHTTP} = require('express-graphql')
 const {buildSchema} = require('graphql')
 const mongoose = require('mongoose')
-
+const Event = require('./models/event')
 
 const app = express()
-
-// mock data to test mutation
-const events = [];
 
 app.use(bodyParser.json())
 
@@ -43,23 +40,40 @@ app.use('/graphql', graphqlHTTP({
         }`),
     rootValue: {
         events: _=> events,
+
+
+        // Create event Resolver _________________________________________________
         createEvent: (args)=>{
-            const event ={
-                _id: Math.random().toString(),
-                title:args.eventInput.title, 
+            const event = new Event({
+                title:args.eventInput.title,
                 description: args.eventInput.description,
                 price: +args.eventInput.price,
-                date: args.eventInput.date
-            }
-            events.push(event)
-            // console.log(events)
-            return events
-        } 
+                date: new Date(args.eventInput.date)
+            });
+
+            /* must return the event here, for graphQL to know that resolver executes an async operation
+               and wait for it before it continues with the code bellow */
+            return event
+                    .save()
+                    // save() is provided by the mongoose package
+                    .then(res => {
+                        console.log(res)
+                        return {...res._doc}
+                        /* this return .. will leave the meta date behind from the object, 
+                        and will return the new added event, using '._doc' */ 
+                    })
+                    .catch(err => {
+                        console.log(err)
+                        throw err
+                        //must throw and error that graphql can process
+                    });
+        }
+        // end of Create event Resolver __________________________________________
     },
     graphiql: true
 }))
 
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.rhd9l.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`)
-.then(console.log('connected to dataBase'), _=> app.listen(3000))
+.then( _=> app.listen(3000), console.log('connected to dataBase'))
 .catch(err => {console.log(err)})
 
