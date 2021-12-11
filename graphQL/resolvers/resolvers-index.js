83f440d92d68b942c892ad1a1fe59f33f62cb49d
John-v77 <john.vs11@hotmail.com>
2021-12-10 21:53:22 -0500

    restructuring - changing to async

--- graphQL/resolvers/resolvers-index.js
+++ graphQL/resolvers/resolvers-index.js
@@ -6,29 +6,33 @@ const User = require('../../models/user')
 
 
 // function to be used to retrieve the user info in the resolver
-const user = userId => {
-    return User.findById(userId)
-    .then(user => { 
-        return {
-            ...user._doc,
-             _id :user.id,
-            createdEvents: events.bind(this, createdEvents:events.bind(this, user._doc.createEvents))    
-            }
-        })
-    .catch(err => { throw err })
+const user = async userId => {
+    try {
+    const user = await User.findById(userId)
+    return {
+        ...user._doc,
+        _id :user.id,
+        createdEvents: events.bind(this, user._doc.createEvents)
+    }
+    }catch(err) { throw err }
 }
 
 
 // function to be used to retrieve the events info in the resolver
-const events = eventIds => {
-    return Event.find({_id: {$in: eventIds}}) // $in - special operator in mangoDb
-                .then(events => {
-                    return events.map(event =>{
-                        return {...event._doc,
-                                 _id: event.id,
-                                creator:user.bind(this, event.creator)}
-                    })
-                })
+const events = async eventIds => {
+
+    try {
+        const events = await Event.find({_id: {$in: eventIds}}) // $in - special operator in mangoDb
+                
+        return events.map(event =>{
+            return {
+                    ...event._doc,
+                    _id: event.id,
+                    date: new Date(event._doc.date).toISOString(),
+                    creator:user.bind(this, event.creator)
+            }
+        })
+    } catch(err){ throw err}     
 }
 
 
@@ -36,23 +40,24 @@ const events = eventIds => {
 
 module.exports = {
 
-    events: _=> {
-        return Event.find()
-        // return is for async await
+    events: async _=> {
+
+        try{
+        const events = await Event.find()
             .populate('creator')
-            .then(ev => {
-                return ev.map(each => {
-                    return {
-                        ...each._doc,
-                        _id:each.id,
-                        creator:user.bind(this, each._doc.creator)
-                    }
-                })
+            return ev.map(each => {
+                return {
+                    ...each._doc,
+                    _id:each.id,
+                    date: newDate(each._doc.date).toISOString(),
+                    creator:user.bind(this, each._doc.creator)
+                }
             })
-            .catch(err => { throw err })
+
+        } catch(err) { throw err }
     },
     // Create event Resolver _________________________________________________
-    createEvent: (args)=>{
+    createEvent: async (args)=>{
         const event = new Event({
             title:args.eventInput.title,
             description: args.eventInput.description,
@@ -61,61 +66,46 @@ module.exports = {
             creator: '5c0d'
         })
 
-        /* must return the event here, for graphQL to know that resolver executes an async operation
-           and wait for it before it continues with the code bellow */
         let createdEvent
-        return event
-                .save()
-                // save() is provided by the mongoose package
-                .then(res => {
+        try{
+            const result = await event.save()
+            // save() is provided by the mongoose package
+            createdEvent = {...res._doc,
+                            _id: res._doc._id.toString(),
+                            date: new Date(event._doc.date).toISOString(),
+                            creator: user.bind(this, res._doc.creator)
+                            }
 
-                    createdEvent = {...res._doc,
-                                     _id: res._doc._id.toString(),
-                                    creator: user.bind(this, res._doc.creator)
-                                    }
-                                    
-                    return User.findById('5c0d')
-                })
-                .then(user => {
-                    if(!user){
-                        throw new Error('User not found.')
-                    }
-                    //or 
-                    // user || throw new Error('User not found.')
-                    user.createEvent.push(event)
-                    return user.save()
-                })
-                .then(res =>{
-                    return {...res._doc, _id: res._doc._id.toString()}
-                })
-                .catch(err => { 
-                    console.log(err)
-                    throw err })
-                    //must throw and error that graphql can process
-                
+            const creator = await User.findById('5c0d')
+            if(!creator){ throw new Error('User not found.') }
+
+
+            creator.createEvent.push(event)
+            await creator.save()
+
+            return createdEvent
+        } catch(err) { throw err }
     },
     // end of Create event Resolver __________________________________________
 
     // Create User Resolver __________________________________________________
-    createUser: args =>{
-        return User.findOne({email:args.userInput.email})
-                    .then(user =>{
-                        if(user) throw new Error('User exists already.')
+    createUser: async args =>{
+        try{
+            const existingUser = await User.findOne({email:args.userInput.email})
+            if(existingUser) throw new Error('User exists already.')
 
-                        return bcrypt.hash(args.userInput.password, 12)
-                    })
-                            
-                    .then(hashedPassword =>{
-                        const user = new User({
-                            email: args.userInput.email,
-                            password: args.userInput.password
-                        })
-                        return user.save()
-                    })
-                    .then(res =>{
-                        return {... res._doc, password:null,  _id: res.id}    
-                    })
-                    .catch(err =>{ throw err })
+            const hashedPassword =  await bcrypt.hash(args.userInput.password, 12)
+
+            const user = new User({
+                email: args.userInput.email,
+                password: args.userInput.password
+            })
+
+            const result = await user.save()
+
+            return {... res._doc, password:null,  _id: res.id}    
+
+        }catch(err) { throw err }
     }
     // end of User Resolver __________________________________________________
 }
