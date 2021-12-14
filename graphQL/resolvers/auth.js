e18e564ef8c0425ec0c4864a6cea781632229838
John-v77 <john.vs11@hotmail.com>
2021-12-14 18:35:14 -0500

    adds Authentication

--- graphQL/resolvers/auth.js
+++ graphQL/resolvers/auth.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcryptjs')
 const User = require('../../models/user')
-
+const jwt = require('jsonwebtoken')
 module.exports = {   
     // Create User Resolver __________________________________________________
     createUser: async args =>{
@@ -20,6 +20,24 @@ module.exports = {
             return {...result._doc, password:null,  _id: result.id}    
 
         }catch(err) { throw err }
-    },
-    // end of User Resolver __________________________________________________
+    },  // end of User Resolver __________________________________________________
+
+
+    //this name has to match the name in "type RootQuery" schema-index.js
+    login: async({email, password}) => {
+        const user = await User.findOne({email:email})
+        if(!user) throw new Error('Invalid Cridentials: user not found')
+
+        const isEqual = await bcrypt.compare(password, user.password)
+        if(!isEqual) throw new Error('Invalid Cridentials: password is incorrect')
+
+        const token = await jwt.sign(
+                                {userId: user.id, email: user.email},
+                                'superLongerSuperSecretKey',
+                                {expriresIn:'1h'}) 
+        return { 
+                userId: user.id, 
+                token: token, 
+                tokenExpiration: 1}
+    }
 }
