ae32fc8ec2b29c58a782aad650ae8dddf82f153b
John-v77 <john.vs11@hotmail.com>
2021-12-24 10:12:40 -0500

    new menu based the state of the userToken

--- client/src/components/nav/NavBar.js
+++ client/src/components/nav/NavBar.js
@@ -7,7 +7,7 @@ function NavBar(props) {
 
     const [user, setUser] = useContext(UserContext)
 
-    console.log(user, 'nav')
+    console.log(user.token, 'nav')
 
     return (
         <header className='header'>
@@ -17,9 +17,9 @@ function NavBar(props) {
 
             <nav className='navBar'>
                 <ul>
-                    <li> <Link to='/auth'>      Login   </Link> </li>
-                    <li> <Link to='/events'>    Events   </Link> </li>
-                    <li> <Link to='/booking'>   Booking   </Link> </li>
+                    {!user.token &&     <li> <Link to='/auth'>  Login  </Link> </li>}
+                                        <li> <Link to='/events'> Events </Link> </li>
+                    {user.token &&      <li> <Link to='/booking'> Booking </Link> </li>}
                 </ul>
             </nav>
         </header>
