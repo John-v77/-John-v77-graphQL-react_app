4dd29aef9b56e745d463cba97be7bfde6cd75c5d
John-v77 <john.vs11@hotmail.com>
2021-12-24 11:17:59 -0500

    events component  init :  adding the modal

--- client/src/components/events/Events.js
+++ client/src/components/events/Events.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Backdrop from '../backdrop/Backdrop';
+import Modal from '../modal/Modal';
+import './events.css'
 
 function Events(props) {
+
+    const [creatingEvent, setCreatingEvent] = useState(false)
+
+    const createEventHandler =()=>{setCreatingEvent(true)}
+
+    const modalCancel =()=>{setCreatingEvent(false)}
+
+    const modalConfirm =()=>{setCreatingEvent(false)}
+
+
     return (
         <div>
-            
+            <>  
+                {creatingEvent && <Backdrop/>}
+                {creatingEvent && 
+                                <Modal title="Add Event" canCancel canConfirm onCancel={modalCancel} onConfirm={modalConfirm}>
+                                    <p>Model Content</p>
+                                </Modal>
+                }
+                <div className='events-control'>
+                    <p>Share your own Events!</p>
+                    <button className='btn' onClick={createEventHandler}> Create Event</button>
+                </div>
+            </>
         </div>
     );
 }
